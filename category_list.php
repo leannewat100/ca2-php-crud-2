13ed3c00de5ecb33c0f1ee1371dcb5681ad5a6f5
Leanne Watters <leannewat100@gmail.com>
2023-01-09 23:35:15 +0000

    changing category table to dark and bordered

--- category_list.php
+++ category_list.php
@@ -14,23 +14,26 @@
 <?php
 include('includes/header.php');
 ?>
+<br>
     <h1>Category List</h1>
     
-    <div class="table-responsive categorieslist">
-    <table>
+    <div class="table-responsive cate">
+    <table class="table table-bordered-dark">
+        
         <tr>
             <th>Name</th>
             <th>&nbsp;</th>
         </tr>
+      
         <?php foreach ($categories as $category) : ?>
         <tr>
             <td><?php echo $category['categoryName']; ?></td>
             <td>
-                <form action="delete_category.php" method="post"
+                <form class="table" action="delete_category.php" method="post"
                       id="delete_product_form">
                     <input type="hidden" name="category_id"
                            value="<?php echo $category['categoryID']; ?>">
-                    <input class="btn btn-light" type="submit" value="Delete">
+                    <input class="btn btn-light delbtn" type="submit" value="Delete">
                 </form>
             </td>
         </tr>
@@ -42,10 +45,14 @@ include('includes/header.php');
     <h2>Add Category</h2>
     <form action="add_category.php" method="post"
           id="add_category_form">
-
-        <label>Name:</label>
-        <input type="input" name="name" placeholder="Category Name" required>
-        <input id="add_category_button" type="submit" class="btn btn-light" value="Add">
+          <div class="row">
+          <div class="col-md-4">
+        <input type="input" class="form-control" name="name" placeholder="Category Name" required>
+        </div>
+        <div class="col-sm-2">
+        <input id="add_category_button" class="form-control" type="submit" class="btn btn-light" value="Add">
+        </div>
+        </div>
     </form>
     <br>
 
